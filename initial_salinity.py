Fix bug in salt ic and ocean bc, scaling for evap.

initial_salinity.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 
+from stompy.spatial import interp_4d
+
 from stompy.grid import unstructured_grid
 from stompy.model.delft import dfm_grid
 from stompy.spatial import wkb2shp
@@ -120,8 +122,10 @@ def initial_salinity_dyn(run_base_dir,
                          parse_dates=['Datetime'])
         sfei_salt=sfei['S_PSU']
         valid=~(sfei_salt.isnull())
-        sfei_salt_now=np.interp(utils.to_dnum(run_start),
-                                utils.to_dnum(sfei.Datetime[valid]),sfei_salt[valid])
+        # limit to data within 20 days of the request
+        sfei_salt_now=utils.interp_near(utils.to_dnum(run_start),
+                                        utils.to_dnum(sfei.Datetime[valid]),sfei_salt[valid],
+                                        max_dx=20.0)
         geom=obs_shp['geom'][ np.nonzero(obs_shp['name']==name)[0][0] ]
         xy=np.array(geom)
         if np.isfinite(sfei_salt_now):
@@ -130,42 +134,28 @@ def initial_salinity_dyn(run_base_dir,
 
     ##
 
-    xy=np.array(mooring_xy)
-
-    sfei_init_salt=np.c_[xy[:,0],xy[:,1],mooring_salt]
-
+    if len(mooring_xy):
+        xy=np.array(mooring_xy)
+        sfei_init_salt=np.c_[xy[:,0],xy[:,1],mooring_salt]
+        init_salt=np.concatenate( (usgs_init_salt,
+                                   sfei_init_salt) )
+    else:
+        init_salt=usgs_init_salt
     ##
 
-    init_salt=np.concatenate( (usgs_init_salt,
-                               sfei_init_salt) )
+    # try again, but with the interp_4d code:
+    samples=pd.DataFrame()
+    samples['x']=init_salt[:,0]
+    samples['y']=init_salt[:,1]
+    samples['value']=init_salt[:,2]
+    # doesn't really matter
+    samples['weight']=1e3*np.ones_like(init_salt[:,0])
 
-    ##
-
-
-    differ=unstructured_diffuser.Diffuser(grid=g)
-
-    ##
-
-    for x,y,salt in init_salt:
-        try:
-            differ.set_dirichlet(salt,xy=[x,y],on_duplicate='skip')
-        except differ.PointOutsideDomain as exc:
-            print(exc)
-            continue
-
-    ##
-    differ.construct_linear_system()
-    differ.solve_linear_system(animate=False)
-
-    ##
+    salt=interp_4d.weighted_grid_extrapolation(g,samples,alpha=5e-4)
 
     cc=g.cells_centroid()
 
-    ##
-
-    cc_salt=np.concatenate( ( cc, differ.C_solved[:,None] ),axis=1 )
-
-    ##
+    cc_salt=np.concatenate( ( cc, salt[:,None] ),axis=1 )
 
     # Because DFM is going to use some interpolation, and will not reach outside
     # the convex hull, we have to be extra cautious and throw some points out farther

ocean.py
@@ -80,16 +80,11 @@ def add_ocean(run_base_dir,
 
         water_level=utils.fill_tidal_data(tides.water_level)
 
-        if 0: # FIR filter, has to be shorter to avoid attenuation
-            # And lowpass at 1 hour to get rid of wave energy
-            # with the fir filter, that's about a 2% amplitude loss at 6h.
-            winsize_lp=int( np.timedelta64(2,'h') / np.median(np.diff(water_level[:].time)) ) 
-            water_level[:] = filters.lowpass_fir(water_level[:].values,winsize_lp)
-        else: # IIR butterworth.  Nicer, with minor artifacts at ends
-            # 3 hours, defaults to 4th order.
-            water_level[:] = filters.lowpass(water_level[:].values,
-                                             utils.to_dnum(water_level.time),
-                                             cutoff=3./24)
+        # IIR butterworth.  Nicer than FIR, with minor artifacts at ends
+        # 3 hours, defaults to 4th order.
+        water_level[:] = filters.lowpass(water_level[:].values,
+                                         utils.to_dnum(water_level.time),
+                                         cutoff=3./24)
 
         if 1: # apply factor:
             msl= 2.152 - 1.214 # MSL(m) - NAVD88(m) for Point Reyes
@@ -101,9 +96,13 @@ def add_ocean(run_base_dir,
             if lag_seconds!=0.0:
                 # sign:  if lag_seconds is positive, then I want the result
                 # for time.values[0] to come from original data at time.valules[0]-lag_seconds
-                water_level[:] = np.interp( utils.to_dnum(tides.time.values),
-                                            utils.to_dnum(tides.time.values)-lag_seconds/86400.,
-                                            tides.water_level.values )
+                if 0: # Why interpolate here? Just alter the timebase.
+                    water_level[:] = np.interp( utils.to_dnum(tides.time.values),
+                                                utils.to_dnum(tides.time.values)-lag_seconds/86400.,
+                                                tides.water_level.values )
+                else:
+                    # Adjust time base directly.
+                    water_level.time.values[:] = water_level.time.values + np.timedelta64(lag_seconds,'s')
             
         if 'water_temperature' not in tides:
             log.warning("Water temperature was not found in NOAA data.  Will use constant 15")

precip_evap.py
@@ -4,7 +4,11 @@ A first cut at direct precipitation and evaporation.
 This depends on '/opt/data/cimis/union_city-hourly-2001-2016.nc'
 which is currently downloaded through 2017-01-08 on hpc.
 """
+import os
+import datetime
+
 import xarray as xr
+import numpy as np
 from stompy import utils
 
 # Last SUNTANS run had used NARR
@@ -27,10 +31,59 @@ def load_cimis():
                       [-1,1],[0.7,0.6])
     union_city['HlyEvap']=union_city.HlyEto/factors
 
+    union_city.time.values += np.timedelta64(8,'h')
     return union_city
 
 # Data are in mm/hour
 # relevant fields are union_city.time, union_city.HlyEvap, and union_city.HlyPrecip
+# Times are adjusted from assumed PST to UTC
 
 
-def add_cimis_evap_precip(run_base_dir,run_start,run_stop,old_bc_fn):
+def mdu_time_range(mdu):
+    t_ref=utils.to_dt64( datetime.datetime.strptime(mdu['time','RefDate'],'%Y%m%d') )
+
+    if mdu['time','Tunit'].lower() == 'm':
+        tunit=np.timedelta64(1,'m')
+    else:
+        raise Exception("TODO: allow other time units")
+    
+    t_start = t_ref+int(mdu['time','tstart'])*tunit
+    t_stop = t_ref+int(mdu['time','tstop'])*tunit
+    return t_ref,t_start,t_stop
+
+def add_cimis_evap_precip(run_base_dir,mdu,scale_precip=1.0,scale_evap=1.0):
+    data = load_cimis()
+
+    pad=np.timedelta64(5,'D')
+
+    t_ref,t_start,t_stop=mdu_time_range(mdu)
+
+    old_bc_fn=os.path.join(run_base_dir,mdu['external forcing','ExtForceFile'])
+
+    with open(old_bc_fn,'at') as fp:
+        # some mentions of "rainfall", other times "rain"
+        lines=["QUANTITY=rainfall",
+               "FILENAME=precip_evap.tim",
+               "FILETYPE=1", # uniform
+               "METHOD=1", # ?? interpolate in space and time?
+               "OPERAND=O",
+               ""]
+        fp.write("\n".join(lines))
+
+    sel=((data.time>=t_start-pad)&(data.time<=t_stop+pad)).values
+    minutes=(data.time.values[sel] - t_ref) / np.timedelta64(60,'s')
+
+    # starting unit: mm/hr
+    # What unit do they want? some mentions in the source of mm/hr, other times
+    # mm/day.  I think they want mm/day.  Initial look at the rain values in the
+    # output confirm that units are good.
+    net_precip=24*(scale_precip*data.HlyPrecip.values[sel] - scale_evap*data.HlyEvap.values[sel])
+
+    time_series = np.c_[minutes,net_precip]
+    
+    np.savetxt(os.path.join(run_base_dir,'precip_evap.tim'),
+               time_series,
+               fmt="%g")
+        
+
+        